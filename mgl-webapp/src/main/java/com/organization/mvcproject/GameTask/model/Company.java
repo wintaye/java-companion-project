91f4f5afce942e6ba8746783eb97fb7c1c1ed040
wintaye.gebru <wintaye.gebru@AFSWW6HWVYXGI7.us.accenturefederal.com>
2023-01-20 10:05:54 -0600

    list<Game> no longer initialized at declaration, working

--- mgl-webapp/src/main/java/com/organization/mvcproject/GameTask/model/Company.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/GameTask/model/Company.java
@@ -1,18 +1,15 @@
 
 package com.organization.mvcproject.GameTask.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Component;
 
 public class Company {
 
 	private Long id;
 	private String name;
-	//TODO 1.0 this List doesn't need to be initialized at declaration. 
-	private List<Game> gamesMade = new ArrayList<Game>();
-
+	private List<Game> gamesMade;
+	
+	
 	public Long getId() {
 		return id;
 	}
