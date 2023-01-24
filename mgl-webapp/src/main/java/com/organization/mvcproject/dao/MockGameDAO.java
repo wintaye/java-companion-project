ae9fae865e86ec376eb3fe04daed79d525cf5a53
wintaye.gebru <wintaye.gebru@AFSWW6HWVYXGI7.us.accenturefederal.com>
2023-01-24 09:27:47 -0600

    created dao, removed list from serviceImpl

--- mgl-webapp/src/main/java/com/organization/mvcproject/dao/MockGameDAO.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/dao/MockGameDAO.java
@@ -1,5 +1,65 @@
 package com.organization.mvcproject.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.organization.mvcproject.model.Game;
+
+@Repository
 public class MockGameDAO {
+	
+	private static Long gameId = new Long(0);
+	private static Long companyId = new Long(0);
+	private static List<Game> games = new ArrayList<Game>();
+
+	static {
+		games = populateGames();
+	}
+
+	private static List<Game> populateGames() {
+
+		Game game1 = new Game();
+		game1.setId(++gameId);
+		game1.setGenre("Sport");
+		game1.setName("Rocket League");
+
+		Game game2 = new Game();
+		game2.setId(++gameId);
+		game2.setGenre("Shooter");
+		game2.setName("Halo 3");
+
+		Game game3 = new Game();
+		game3.setId(++gameId);
+		game3.setGenre("MMORPG");
+		game3.setName("Runescape");
+
+		games.add(game1);
+		games.add(game2);
+		games.add(game3);
+
+		return games;
+	}
+	
+	
+
+		public Game saveGame(Game game) {
+			game.setId(++gameId);
+			games.add(game);
+			return game;
+	}
+		public List<Game> retrieveAllGames() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		public boolean deleteGame(Long id) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		public Game findGameById(Long id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
 
 }
