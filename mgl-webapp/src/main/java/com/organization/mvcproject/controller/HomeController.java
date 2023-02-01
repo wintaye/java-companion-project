07de07b5e2820d1e3206aaa3976cb66f30571c34
wintaye <41543902+wintaye@users.noreply.github.com>
2023-02-01 15:15:38 -0600

    working, missing Update functionality

--- mgl-webapp/src/main/java/com/organization/mvcproject/controller/HomeController.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.organization.mvcproject.controller;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
